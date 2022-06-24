1944d647a0143721aa1b87bdc888abd2ddcc912c
DoranJosh72 <doranjosh742@gmail.com>
2022-06-24 11:12:43 +0100

    Added Enemy Movement
    I Have found a way for the enemies to move by having them move between two points in various directions

--- The Dungeon Explorer/Assets/Scripts/Patrol.cs
+++ The Dungeon Explorer/Assets/Scripts/Patrol.cs
@@ -4,11 +4,47 @@ using UnityEngine;
 
 public class Patrol : MonoBehaviour
 {
-    
+    private Rigidbody2D rb;
 
+    private Vector3 startPos;
+    public Vector3 endPos;
 
+    private Vector3 targetPos;
 
+    public float speed;
 
+    public int damage;
 
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        startPos = transform.position;
+        targetPos = endPos;
+    }
 
+    void FixedUpdate()
+    {
+        Vector3 currentPos = transform.position;
+
+        if (currentPos == endPos)
+        {
+            targetPos = startPos;
+           // Debug.Log("Reached end, switching target position");
+        }
+        else if (currentPos == startPos)
+        {
+            targetPos = endPos;
+            //Debug.Log("Reached beginning, switching target position");
+        }
+
+        Vector3 targetDirection = (targetPos - currentPos).normalized;
+        rb.MovePosition(currentPos + targetDirection * speed * Time.deltaTime);
+    }
 }
+
+
+
+
+
+
+
